4117ccaeec34de70bfe995ced1af1499a91c44a7
Swapnil64sh <120723989+Studymaster255@users.noreply.github.com>
2023-08-08 21:17:18 +0530

    line from study

--- src/main/java/Sample/Demo2.java
+++ src/main/java/Sample/Demo2.java
@@ -1,13 +1,12 @@
 package Sample;
 
-public class Demo {
+public class Demo2 {
 
 	public static void main(String[] args) {
 		System.out.println("line by study");
 		System.out.println("line by from gui");
 		System.out.println("line added from eclipse");
 		System.out.println("line from GUI 2");
-		System.out.println("line added from eclipse");
 		System.out.println("line added from eclipse");
 	}
 
